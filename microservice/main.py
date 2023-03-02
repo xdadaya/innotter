8263fc8f6051c51548235a4a0e4b52c7120f3219
xdadaya <gerakrah@gmail.com>
2023-03-02 21:07:03 +0300

    middleware

--- microservice/main.py
+++ microservice/main.py
@@ -3,17 +3,22 @@ from microservice.models import PageStatistics
 from microservice.token_verify import TokenVerify
 from microservice.page_statistics_service import PageStatisticsService
 from uuid import UUID
-from typing import Any
 
 
 app = FastAPI()
 
 
-@app.get('/page-statistics/{page_id}', response_model=PageStatistics)
-async def index(page_id: UUID, request: Request) -> Any:
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
     try:
-        token = request.headers["Authorization"]
+        token = request.headers.get("Authorization")
         user_id = TokenVerify.token_verify(token)
+        request.state.uid = user_id
     except KeyError:
         raise HTTPException(status_code=403, detail="No authorization")
-    return PageStatisticsService.get(str(page_id).replace("-", ""), user_id)
+    return await call_next(request)
+
+
+@app.get('/page-statistics/{page_id}', response_model=PageStatistics)
+async def index(page_id: UUID, request: Request) -> PageStatistics:
+    return PageStatisticsService.get(str(page_id).replace("-", ""), request.state.uid)
